661f450b0098f03bc824d89cde3826492d143cf7
guwenjun <guwenjun2017@gmail.com>
2017-08-07 21:26:23 +0800

    an exercise

---
+++ Five/5_5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+    int i_income,i_workday,a;
+    i_income = 0;
+    a = 0;
+
+    printf("请输入工作的总天数:");
+    scanf("%d",&i_workday);
+    while(a <= i_workday){
+	i_income += a;
+	a++;
+    }
+    printf("总收入:%d\n",i_income);
+}
