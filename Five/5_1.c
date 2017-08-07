004327776f4acf65571a6e37891760b0d89435c7
guwenjun <guwenjun2017@gmail.com>
2017-08-07 18:40:23 +0800

    an exercise

--- Five/5_1.c
+++ Five/5_1.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include<stdio.h>                                           //暂留Bug,输入字母无限循环.
 #define PER_HOUR 60
 int main(void)
 {
     int i_minute,i_hour,i_left;
 
-    printf("请输入总分钟数(q键退出):\n");
-    while(scanf("%d",&i_minute) > 0){
+    printf("请输入总分钟数:\n");
+    scanf("%d",&i_minute);
+    while( i_minute> 0){
 	if((i_hour = i_minute/PER_HOUR) > 0)
 	    printf("%dhours,",i_hour);
 	else
@@ -14,6 +15,7 @@ int main(void)
 	    printf("%dminutes\n",i_left);
 	else
 	    printf("%dminute\n",i_left);
+	    scanf("%d",&i_minute);
     }
 	
     return 0;
